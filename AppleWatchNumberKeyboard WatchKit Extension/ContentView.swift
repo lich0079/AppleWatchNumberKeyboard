e1ed3614136cb2bc41273db702bb64dec8e496a1
zhayang <zhayang@ebay.com>
2020-06-07 21:12:35 +0800

    Initial Commit

---
+++ AppleWatchNumberKeyboard WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleWatchNumberKeyboard WatchKit Extension
+//
+//  Created by Zhang, Frank on 2020/6/7.
+//  Copyright © 2020 lich0079. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AppleWatchNumberKeyboard WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  AppleWatchNumberKeyboard WatchKit Extension
+//
+//  Created by Zhang, Frank on 2020/6/7.
+//  Copyright © 2020 lich0079. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
